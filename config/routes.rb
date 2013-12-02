20be872000de03971bbe38267bd696b957a14f4f
John Davison <johncdavison@gmail.com>
2013-12-01 17:37:21 -0800

    debugging google

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Railstutor::Application.routes.draw do
   post 'apply', to: 'main#apply'
   get 'veterans', to: redirect("/")
   get 'register', to: 'main#register'
-  get 'google7df0dfecf855f3f2', to: 'curriculum#google7df0dfecf855f3f2'
   ["views", "controllers", "rubybasics", "models", "mvcroundtrip", "rubyobjects", "overview" ].each do |lesson|
     get "curriculum/#{lesson}", to: "curriculum##{lesson}"
   end
