2af60dba6786f43be9e2af8a0878309eee890799
John Davison <johncdavison@gmail.com>
2013-12-01 21:26:32 -0800

    WIP

--- app/mailers/student_mailer.rb
+++ app/mailers/student_mailer.rb
@@ -7,6 +7,11 @@ class StudentMailer < ActionMailer::Base
     mail(to: @student.email, subject: "Welcome to RubyonRailsTutor.com",  reply_to: JD, bcc: JD)
   end
 
+  def new_lead(student)
+    @student = student
+    mail(to: JD, subject: "rubyonrailstutor new lead")
+  end
+
   def pmt_confirmation(student)
     @student = student
     mail(to: @student.email, subject: "RubyonRailstutor.com PMT Confirmation",  reply_to: JD, bcc: JD)
