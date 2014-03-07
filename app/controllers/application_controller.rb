3d6b2d327c38a1db627fdc6a0b853a08ee59ed11
John Davison <johncdavison@gmail.com>
2014-03-07 09:48:34 -0800

    add cors post from railstutor.github.io

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :set_headers
+
+  private
+
+  def set_headers
+    headers['Access-Control-Allow-Origin'] = 'http://rubyonrailstutor.github.io/'
+    headers['Access-Control-Allow-Methods'] = 'POST'
+  end
 end
